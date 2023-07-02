81e0293e943243055c0cb10e2ab802177943ef7a
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-07-02 16:49:00 +0600

    2. arithmetic operators.java

---
+++ 2. arithmetic operators.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int a= 10;
+        int b= 5;
+
+        System.out.println("Sum: " + (a+ b));
+        System.out.println("Difference: " + (a- b));
+        System.out.println("Product: " + (a* b));
+        System.out.println("Quotient: " + (a / b));
+        System.out.println("Remainder: " + (a% b));
+
+        System.out.println("Incremented value of operand1: " + (++a));
+        System.out.println("Decremented value of operand2: " + (--b));
+    }
+}
