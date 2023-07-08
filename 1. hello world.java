62ca18d00d48ecb1c600e29202f205665413a528
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-07-08 23:52:58 +0600

    1. hello world.java

--- 1. hello world.java
+++ 1. hello world.java
@@ -2,18 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Sujon!");
         System.out.println("Good things take time");
-        System.out.println("Consider operand name a=10 and b=5 ");
         
-        int a= 10;
-        int b= 5;
-
-        System.out.println("Sum: " + (a+ b));
-        System.out.println("Difference: " + (a- b));
-        System.out.println("Product: " + (a* b));
-        System.out.println("Quotient: " + (a / b));
-        System.out.println("Remainder: " + (a% b));
-
-        System.out.println("Incremented value of operand1: " + (++a));
-        System.out.println("Decremented value of operand2: " + (--b));
     }
 }
