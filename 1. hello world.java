4b9ad17178b86dd4eb0c5fdbf84daeabb8f7d129
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-07-08 23:59:49 +0600

    1. hello world.java

--- 1. hello world.java
+++ 1. hello world.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Sujon!\nGood things take time");    
+        System.out.println("Hello, Sujon!\nGood things take time");  
+
+        int a = 1;
+        float b = 3.1416;
+
+        System.out.println("Sum:" + (a+b)); 
     }
 }
