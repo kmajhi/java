baa974fce59f88a09ebb7d27aed640794b396bc6
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-07-09 23:47:07 +0600

    1. hello world.java

--- 1. hello world.java
+++ 1. hello world.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Sujon!\nGood things take time");
-        System.out.println("Welcome to Java");
     }
 }
