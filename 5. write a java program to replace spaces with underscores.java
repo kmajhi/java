77932458d4b92413a5dad6e69f22d4e9783b3175
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-07-11 23:28:38 +0600

    5. write a java program to replace spaces with underscores.java

---
+++ 5. write a java program to replace spaces with underscores.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String name = "Sujon Mahamud Shefat";
+        System.out.println(name.replace(" ", "_"));
+    }
+}
