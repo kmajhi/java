2c91dcc9383560966797cbf8eee69fd6a7d022f4
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-07-16 00:41:55 +0600

    print scalene triangle for n number from user.java

---
+++ print scalene triangle for n number from user.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number for n:>>\t ");
+        int n = sc.nextInt();
+
+        for (int i=n; i>0; i--){
+            for (int j=0; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
+    }
+}
