a4a9a5736e422290c55c34dceed7c5d92b82bd53
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-07-16 02:43:55 +0600

    5. print multiplication table of a given n number in reverse.java

---
+++ basics/5. print multiplication table of a given n number in reverse.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i;
+        System.out.print("Enter the number =>>\t");
+        int n = sc.nextInt();
+
+        for ( i=10; i>=1; i--){
+            System.out.printf("%d x %d \t= \t%d\n", n,i,n*i);
+        }
+    }
+}
