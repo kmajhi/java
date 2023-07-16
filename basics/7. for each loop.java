b6b2c91a1e21ed8f4e20e6e7d822fa782db0e011
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-07-16 19:25:21 +0600

    7. for each loop.java

---
+++ basics/7. for each loop.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] marks = {98,95,96, 95,94,85};
+
+        for (int name : marks){
+            System.out.println(name);
+        }
+    }
+}
