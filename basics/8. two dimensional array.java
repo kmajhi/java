70dab9bac427adcdc6f2f4859ff1320ceb364c74
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-07-16 21:33:34 +0600

    8. two dimensional array.java

--- basics/8. two dimensional array.java
+++ basics/8. two dimensional array.java
@@ -11,8 +11,6 @@ public class Main {
         flats [1][1] = 202;
         flats [1][2] = 203;
 
-
-
         for (int i=0; i< flats.length; i++){
             for (int j=0;  j<flats[i].length; j++){
                 System.out.print(flats[i][j]);
