0dec518090ffd9077aed0610f24eb26790c206f9
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-07-21 00:31:17 +0600

    8. logic method.java

---
+++ 8. logic method.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    static int logic(int x, int y){
+        int z;
+        if (x>y) {
+            z = x+y;
+        } else {
+            z = (x+y)*5;
+        }
+        return z;
+    }
+
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 7;
+        int c;
+        c = logic(a, b);
+
+        int a1 = 2;
+        int b1 = 1;
+        int c1;
+        c1 = logic(a1, b1);
+
+        System.out.println(c);
+        System.out.println(c1);
+    }
+}
