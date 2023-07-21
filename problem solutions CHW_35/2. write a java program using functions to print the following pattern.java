3cfba5e9569fd0272b6949b319e3ebd785532402
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-07-21 23:33:15 +0600

    2. write a java program using functions to print the following pattern.java
    Expected output:
    
    * 
    * * 
    * * * 
    * * * *

--- problem solutions CHW_35/2. write a java program using functions to print the following pattern.java
+++ problem solutions CHW_35/2. write a java program using functions to print the following pattern.java
@@ -1,7 +1,7 @@
 public class Main{
     static void multiplication(int n){
        for(int i=0; i<n; i++){
-           for (int j=0; j<i+1; j++){
+            for (int j=0; j<i+1; j++){
                System.out.print("* ");
            }
            System.out.println();
