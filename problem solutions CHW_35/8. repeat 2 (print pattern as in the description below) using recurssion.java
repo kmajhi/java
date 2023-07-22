b81c791a00681335cf4fe1da36b4f84a116042fc
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-07-22 22:27:37 +0600

    8. repeat 2 (print pattern as in the description below) using recurssion.java

---
+++ problem solutions CHW_35/8. repeat 2 (print pattern as in the description below) using recurssion.java
@@ -0,0 +1,14 @@
+public class Main {
+    static void pattern(int n){
+        if(n>0){
+            pattern(n - 1);
+            for (int i = 0; i < n; i++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+        public static void main (String[]args){
+        pattern(4);
+        }
+    }
