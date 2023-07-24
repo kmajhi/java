c7bca83a49b0ae91b3937ca02a0e93bcb46fee43
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-07-25 00:38:05 +0600

    1. Constructors overloading.java

---
+++ OOP/1. Constructors overloading.java
@@ -0,0 +1,38 @@
+class Employee{
+    private int id;
+    private String name;
+
+    public Employee(){
+        id = 42;
+        name = "Your-Name-Here";
+    }
+
+    public Employee(String myName, int myId){
+        id = myId;
+        name = myName;
+    }
+
+    public void setId(int i){
+        this.id = i;
+    }
+    public void setName(String n){
+        this.name =n;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Employee sujon = new Employee();
+//        sujon.setName("Sujon");
+//        sujon.setId(39);
+        System.out.println(sujon.getName());
+        System.out.println(sujon.getId());
+    }
+}
