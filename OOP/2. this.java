63076e0b5f6d1fa1cf4bd6d40746d47e62c9ba47
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-07-25 21:09:00 +0600

    2. this.java

---
+++ OOP/2. this.java
@@ -0,0 +1,20 @@
+class EClass{
+    int a;
+    public int getA() {
+        return a;
+    }
+
+    EClass(int a){
+       this.a = a;
+    }
+
+//    public int ReturnOne(){
+//        return 1;
+//    }
+}
+public class Main{
+    public static void main(String[] args) {
+        EClass e = new EClass(5);
+        System.out.println(e.getA());
+    }
+}
