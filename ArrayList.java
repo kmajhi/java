8c770376b9b4d190243cc70e81d2907849a20007
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-08-05 10:29:28 +0600

    ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> number = new ArrayList<Integer>();
+        number.add(10);
+        number.add(20);
+        number.add(30);
+        number.add(40);
+        System.out.println(number);
+
+    }
+}
