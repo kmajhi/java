86aa02e97df5051346c42248bf12298c92e63b52
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-10-06 23:55:38 +0600

    1. date and time.java

---
+++ time-date/1. date and time.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        String formattedDateTime = currentDateTime.format(formatter);
+        System.out.println("Current Date and Time: " + formattedDateTime);
+    }
+}
