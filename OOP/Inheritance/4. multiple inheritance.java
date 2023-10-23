3c7c77784e01ac510374f8df450a5478d734c8ac
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-10-24 00:24:53 +0600

    4. multiple inheritance.java

---
+++ OOP/Inheritance/4. multiple inheritance.java
@@ -0,0 +1,35 @@
+/* multiple inheritance is not supported in java. 
+  (To reduce the complexity and simplify the language, 
+  multiple inheritance is not supported in java.) 
+  
+  
+  Consider a scenario where A, B, and C are three classes. 
+  The C class inherits A and B classes. If A and B classes have 
+  the same method and you call it from child class object, there 
+  will be ambiguity to call the method of A or B class.
+
+Since compile-time errors are better than runtime errors, 
+Java renders compile-time error if you inherit 2 classes. 
+So whether you have same method or different, 
+there will be compile time error.*/
+
+
+class A{  
+void msg(){
+  System.out.println("Hello");
+  }  
+}  
+class B{  
+void msg(){
+  System.out.println("Welcome");
+  }  
+}  
+class C extends A,B{
+  //suppose if it were  
+  
+ public static void main(String args[]){  
+   C obj=new C();  
+   obj.msg();//Now which msg() method would be invoked?  
+}  
+}
+
