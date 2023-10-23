4cde0c483c78f5bdbac48983dddb7c7ecc491979
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-10-24 00:55:54 +0600

    1. Method Overloading changing number of arguments.java

---
+++ OOP/Polymorphism/Method Overloading/1. Method Overloading changing number of arguments.java
@@ -0,0 +1,17 @@
+// static methods so that we don't need to create instance for calling methods.
+
+class Adder{  
+  static int add(int a,int b){
+    return a+b;
+}  
+static int add(int a,int b,int c){
+  return a+b+c;
+  }  
+}  
+
+class TestOverloading1{  
+  public static void main(String[] args){  
+    System.out.println(Adder.add(11,11));  
+    System.out.println(Adder.add(11,11,11));  
+  }
+}  
