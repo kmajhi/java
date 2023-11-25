266d91cb5ac39dba04141a478c42585d300c8acf
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-11-25 10:55:08 +0600

    Java Varargs Simple Addition Solution.java

--- Hackerrank Problem Solving/Java Varargs Simple Addition Solution.java
+++ Hackerrank Problem Solving/Java Varargs Simple Addition Solution.java
@@ -1,21 +1,15 @@
 class Add {
-    int sum;
-    void add(int a, int b){
-         sum = a+b;
-        System.out.println(a+"+"+b+"="+sum);
-    }
-    void add(int a, int b, int c){
-         sum = a+b+c;
-        System.out.println(a+"+"+b+"+"+c+"="+sum);
-    }
-
-    void add(int a, int b, int c, int d, int e){
-         sum = a+b+c+d+e;
-        System.out.println(a+"+"+b+"+"+c+"+"+d+"+"+e+"="+sum);
-    }
-    void add(int a, int b, int c, int d, int e, int f){
-        int sum = a+b+c+d+e+f;
-        System.out.println(a+"+"+b+"+"+c+"+"+d+"+"+e+"+"+f+"="+sum);
+     void add(int ...arr){
+        int sum=0;
+        String s="";
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            s+=arr[i];
+            if(i<arr.length-1)
+                s+="+";
+        }
+        s+="="+sum;
+        System.out.println(s);
     }
 }
 public class Solution {
@@ -27,5 +21,3 @@ public class Solution {
         ob.add(1,2,3,4,5,6);
     }
 }
-
-
