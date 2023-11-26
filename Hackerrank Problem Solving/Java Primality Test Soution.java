340e3e30fcc85a20a812822eaeb451636347b0be
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-11-26 23:46:35 +0600

    Java Primality Test Soution.java

---
+++ Hackerrank Problem Solving/Java Primality Test Soution.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        bufferedReader.close();
+        BigInteger p = new BigInteger(n);
+        if(p.isProbablePrime(13)){
+            System.out.println("prime");
+        }else{
+            System.out.println("not prime");
+        }
+    }
+}
