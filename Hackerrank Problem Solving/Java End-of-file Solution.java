05ba4e117250671ff7d5f25bf8442cb5fea9824e
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-11-27 23:52:37 +0600

    Java End-of-file Solution.java

---
+++ Hackerrank Problem Solving/Java End-of-file Solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int line_number =1;
+        while(sc.hasNext()){
+            System.out.println(line_number+" "+sc.nextLine());
+            line_number++;
+        }
+        sc.close();
+    }
+}
